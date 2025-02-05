f86287a153b4448ab5505c4472caa1a2ecdca206
Shuhrat666 <shuhratodilbekov3@gmail.com>
2025-02-05 21:04:26 +0500

    Update mig31.php

--- mig31.php
+++ mig31.php
@@ -1,3 +1,4 @@
+<?php include 'includes/db.php'; ?>
 <?php
 $pdo = new PDO('mysql:host=localhost;dbname=myblog', 'root', '$Huhrat333');
 $stmt=$pdo->prepare(query:"create table blog(id int primary key auto_increment, title varchar(256), text varchar(2048), created_at datetime)");
