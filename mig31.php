f731906ebe4a35b430e313753fd4dc09059deb7c
Shuhrat666 <shuhratodilbekov3@gmail.com>
2025-02-05 11:51:39 +0500

    Add files via upload

---
+++ mig31.php
@@ -0,0 +1,6 @@
+<?php
+$pdo = new PDO('mysql:host=localhost;dbname=myblog', 'root', '$Huhrat333');
+$stmt=$pdo->prepare(query:"create table blog(id int primary key auto_increment, title varchar(256), text varchar(2048), created_at datetime)");
+$stmt->execute();
+printf("Created successsfully !\n");
+?>
